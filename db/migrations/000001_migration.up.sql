d68a2165e7f4412b2d0ea2207b1cbb231316dc8b
onao13 <onao13@outlook.com>
2024-01-22 21:51:20 +0300

    Добавлены файлы миграции БД

---
+++ db/migrations/000001_migration.up.sql
@@ -0,0 +1,31 @@
+create table if not exists humans
+(
+    id         bigserial not null
+    constraint table_name_pk
+    primary key,
+    name       varchar(50)                                           not null,
+    surname    varchar(50)                                           not null,
+    patronymic varchar(50)                                           not null,
+    age        integer                                               not null,
+    gender     varchar(6)                                            not null
+    );
+
+create table if not exists humans_nationality
+(
+    humanid     bigint      not null
+    constraint humans_nationality_humans_id_fk
+    references humans,
+    countryid   varchar(10) not null,
+    probability real        not null
+    );
+
+create index human__surname__index
+    on humans (surname);
+
+alter table humans_nationality
+drop constraint humans_nationality_humans_id_fk;
+
+alter table humans_nationality
+    add constraint humans_nationality_humans_id_fk
+        foreign key (humanid) references humans
+            on delete cascade;
